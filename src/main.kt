b6d75d880abeac7a53f8c7642c90b522a4d2c453
Grace Waceke <gracewacekemwangi@gmail.com>
2022-03-29 16:33:14 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,47 @@
+fun main(){
+    var carName= "Volvo"
+    println(carName)
+
+    val birthYear= 1975
+    println(birthYear)
+
+    var b= addition (30, 30 )
+    println(b)
+
+    var greeting= "Hello"
+    println(greeting)
+
+    var txt="Hello World"
+    println(txt)
+    var txt1="Hello World"
+    var txt2="Hello World"
+    println(txt1.compareTo(txt2))
+
+
+        println("Hello World")
+    var a="helloword"
+    if ("a"=="b"){
+        println("not equal to")
+        } else{
+        println("equal to")
+    }
+
+
+
+
+
+
+}
+fun addition(x:Int, y:Int): Int{
+    var sum= x + y
+    println(sum)
+    return sum
+    //what data type is x
+    //val x=5
+//ANSWER: 5 is an Integer
+    // what data type is "Hello World" this is a String
+// Insert the missing part to access the first element in the string var text= "Hello World"
+
+
+
+}
